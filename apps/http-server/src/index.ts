4f9fb69edb13b15703830ac73ec6e8c0abd1f7ec
anukoolvikram <anukoolvikram777@gmail.com>
2025-07-03 16:01:41 +0530

    changed fe

--- apps/http-server/src/index.ts
+++ apps/http-server/src/index.ts
@@ -5,7 +5,7 @@ const app=express()
 app.use(express.json())
 
 app.get('/', (req,res)=>{
-    res.send('Hello')
+    res.send('Hello bro this is http server')
 })
 
 app.post('/signup', async (req,res)=>{
--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,12 +1,20 @@
-import {client} from '@repo/prisma/client'
+import { client } from '@repo/prisma/client';
 
 export default async function Home() {
-  const user=await client.user.findFirst()
+  const users = await client.user.findMany();
 
   return (
     <div>
-      <div>{user?.username}</div>
-      <div>{user?.password}</div>
+      <h1>Welcome to anukool.info</h1>
+      <h2>User List:</h2>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            <strong>Name:</strong> {user.username} <br />
+            <strong>Password:</strong> {user.password}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
